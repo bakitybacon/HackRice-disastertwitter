1c1f99c16fd0f18c328b75b2129117714f40e826
potatopeno <mateo.dagaro@comcast.net>
2017-09-22 20:58:42 -0500

    Added csv file, began writing search function.

--- twitterscraper.py
+++ twitterscraper.py
@@ -1,11 +1,12 @@
 import twitter
 import csv
+import re
 
 class TwitterScraper(twitter.Api):
     """
     Scraps twitter for disaster information. Stores any information in a database.
     """
-    def __init__(self,consumer_key,consumer_secret,access_token_key,access_token_secret, ):
+    def __init__(self,consumer_key,consumer_secret,access_token_key,access_token_secret):
         super().__init__(consumer_key=consumer_key,
                          consumer_secret=consumer_secret,
                          access_token_key=access_token_key,
@@ -15,5 +16,17 @@ class TwitterScraper(twitter.Api):
         """
         Takes a list of key word strings and searches each of them on twitter.
         """
+
         for keyword in keywords:
             results = self.GetSearch(term=keyword,geocode=geocode,count=25)
+            with open("./data.csv", "a") as data:
+                writer = csv.writer(data)
+                writer.writerow(self.parse_status(results,keywords))
+
+
+    def parse_status(self,status,keywords):
+        """
+        Takes a twitter.Status and parse's it for location and severity. Returns a tuple with latitude, longitude, and
+        severity.
+        """
+        pass
