3638318edb10707a06ed76ef9e80987a28789d52
potatopeno <mateo.dagaro@comcast.net>
2017-09-23 14:38:03 -0500

    Finally got it working. Started working on a config file.

--- DisasterTwitter/twitterscraper.py
+++ DisasterTwitter/twitterscraper.py
@@ -7,21 +7,28 @@ class TwitterScraper(twitter.Api):
     """
     Scraps twitter for disaster information. Stores any information in a database.
     """
-    def __init__(self, consumer_key, consumer_secret, access_token_key,access_token_secret):
-        super().__init__(consumer_key=consumer_key,
-                         consumer_secret=consumer_secret,
-                         access_token_key=access_token_key,
-                         access_token_secret=access_token_secret)
+    def __init__(self, config):
+        super().__init__(consumer_key=config['consumer_key'],
+                         consumer_secret=config['consumer_secret_key'],
+                         access_token_key=config['access_token'],
+                         access_token_secret=config['access_token_secret'])
+        self._sconfig = config
 
-    def search_keywords(self,keywords,geocode):
+    def search_keywords(self):
         """
         Takes a list of key word strings and searches each of them on twitter.
         """
-
-        results = self.GetSearch(term=keywords[0],geocode=geocode,count=25)
-        with open("./data.csv", "a") as data:
-            writer = csv.writer(data)
-            writer.writerow(self.parse_status(results,keywords))
+        for keyword in self._sconfig['keywords']:
+            geocode=self._sconfig['geocode']+','+ self._sconfig['radius']
+            results = self.GetSearch(term=keyword,count=100,geocode=geocode)
+            print(len(results))
+            for x in results:
+                if not x.coordinates:
+                    continue
+                print(x.coordinates)
+                #with open("./data.csv", "a") as data:
+                #    writer = csv.writer(data)
+                #    writer.writerow(self.parse_status(results,keywords))
 
 
     def parse_status(self,status,keywords):
